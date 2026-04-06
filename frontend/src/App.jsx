755b481118d7bdb22404307ecb3530225a563f6c
Nadav Jacobs <nadavjac@gmail.com>
2026-04-06 22:05:21 +0300

    feat: print QR code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,32 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar.jsx';
 import LocationsPage from './pages/LocationsPage.jsx';
 import LocationDetailPage from './pages/LocationDetailPage.jsx';
 import ContainerDetailPage from './pages/ContainerDetailPage.jsx';
 import ScanContainerPage from './pages/ScanContainerPage.jsx';
+import PrintContainerPage from './pages/PrintContainerPage.jsx';
 import SearchPage from './pages/SearchPage.jsx';
 
-export default function App() {
+function AppLayout() {
   return (
     <div className="app">
       <Navbar />
-      <Routes>
+      <Outlet />
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <Routes>
+      <Route element={<AppLayout />}>
         <Route path="/" element={<LocationsPage />} />
         <Route path="/locations/:id" element={<LocationDetailPage />} />
         <Route path="/containers/:id" element={<ContainerDetailPage />} />
         <Route path="/scan/container/:token" element={<ScanContainerPage />} />
         <Route path="/search" element={<SearchPage />} />
-      </Routes>
-    </div>
+      </Route>
+      <Route path="/print/container/:token" element={<PrintContainerPage />} />
+    </Routes>
   );
 }
